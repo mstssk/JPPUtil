3761a3613e295ae5ee63c41e6c466f52c66fb3f9
mstssk <mstssk+github@gmail.com>
2011-06-26 01:35:06 +0900

    パース処理を正規表現での実装に変更。ミリ秒の省略に対応するため。

--- src/jp/mstssk/util/jsonpullparser/util/converter/DateConverter.java
+++ src/jp/mstssk/util/jsonpullparser/util/converter/DateConverter.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.io.Writer;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.vvakame.util.jsonpullparser.JsonFormatException;
 import net.vvakame.util.jsonpullparser.JsonPullParser;
@@ -14,18 +17,26 @@ import net.vvakame.util.jsonpullparser.util.OnJsonObjectAddListener;
 import net.vvakame.util.jsonpullparser.util.TokenConverter;
 
 /**
- * DateConverter
+ * DateConverter<br>
+ * ECMA-262 5th EditionのDate.prototype.toJSONに対応
  * 
  * @author mstssk
  */
 public class DateConverter extends TokenConverter<Date> {
 
 	/**
-	 * ISO-8601<br>
-	 * XXX json2.jsのJSON.stringifyではミリ秒を出力しない
+	 * ISO-8601拡張日付フォーマット<br>
+	 * json2.jsのJSON.stringifyではミリ秒を出力しない
 	 */
-	private static UTCDateFormat dateFormat = new UTCDateFormat(
+	private static final UTCDateFormat dateFormat = new UTCDateFormat(
 			"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+	/**
+	 * 日付パターン<br>
+	 * ミリ秒の省略に対応（ECMA-262 5th Edition # 15.9.1.15 Date Time String Format）
+	 */
+	private static final Pattern datePattern = Pattern
+			.compile("^(\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2})(?:.(\\d{3}))?Z$");
 
 	/**
 	 * getInstance
@@ -50,7 +61,7 @@ public class DateConverter extends TokenConverter<Date> {
 				// null
 				break;
 			case VALUE_STRING:
-				date = parseDateString(parser.getValueString());
+				date = parseDateStr(parser.getValueString());
 				break;
 			default:
 				throw new JsonFormatException("Illegal format for date string.");
@@ -69,12 +80,22 @@ public class DateConverter extends TokenConverter<Date> {
 	@Override
 	public void encodeNullToNull(Writer writer, Date obj) throws IOException {
 
-		if (obj == null) {
-			JsonUtil.put(writer, obj);
-		} else {
-			JsonUtil.put(writer, dateFormat.format(obj));
-		}
+		JsonUtil.put(writer, formatDateStr(obj));
 
+	}
+
+	/**
+	 * Dateオブジェクトを日付文字列へ
+	 * 
+	 * @param date Date
+	 * @return String
+	 */
+	private String formatDateStr(Date date) {
+		if (date == null) {
+			return null;
+		} else {
+			return dateFormat.format(date);
+		}
 	}
 
 	/**
@@ -84,6 +105,7 @@ public class DateConverter extends TokenConverter<Date> {
 	 * @return Date
 	 * @throws JsonFormatException
 	 */
+	@Deprecated
 	private Date parseDateString(String dateStr) throws JsonFormatException {
 		Date date = null;
 		try {
@@ -92,6 +114,53 @@ public class DateConverter extends TokenConverter<Date> {
 			throw new JsonFormatException(e);
 		}
 		return date;
+	}
+
+	/**
+	 * 日付文字列を正規表現でパース
+	 * 
+	 * @param str JSON日付文字列
+	 * @return Date
+	 * @throws JsonFormatException
+	 */
+	private Date parseDateStr(String str) throws JsonFormatException {
+
+		try {
+
+			Matcher matcher = datePattern.matcher(str);
+			matcher.find();
+			int year = parseInt(matcher.group(1));
+			int month = parseInt(matcher.group(2));
+			int day = parseInt(matcher.group(3));
+			int hour = parseInt(matcher.group(4));
+			int min = parseInt(matcher.group(5));
+			int sec = parseInt(matcher.group(6));
+			int milli = 0;
+			String milliStr = matcher.group(7);
+			if (milliStr != null) {
+				milli = parseInt(milliStr);
+			}
+
+			Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+			cal.set(year, month, day, hour, min, sec);
+			cal.set(Calendar.MILLISECOND, milli);
+
+			return cal.getTime();
+
+		} catch (IllegalStateException e) {
+			throw new JsonFormatException(e);
+		}
+	}
+
+	/**
+	 * Integer.parseInt
+	 * 
+	 * @see java.lang.Integer.parseInt
+	 * @param str String
+	 * @return int
+	 */
+	private int parseInt(String str) {
+		return Integer.parseInt(str);
 	}
 
 	/**
